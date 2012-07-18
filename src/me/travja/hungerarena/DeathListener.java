75fbe3743cb4d1817c75efed6d7c61b28d0592ab
Travis Eggett <the.only.t.craft@gmail.com>
2012-07-17 21:32:21 -0600

    Improvements, and Explosions!

--- src/me/travja/hungerarena/DeathListener.java
+++ src/me/travja/hungerarena/DeathListener.java
@@ -30,16 +30,53 @@ public class DeathListener implements Listener{
 		}
 	}
 	@EventHandler
-    public void onPlayerDeath(PlayerDeathEvent event){
+	public void onPlayerDeath(PlayerDeathEvent event){
 		Player p = event.getEntity();
 		Server s = p.getServer();
 		String pname = p.getName();
+		String leftmsg = ChatColor.BLUE + "There are now " + plugin.Playing.size() + " tributes left!";
 		String[] Spawncoords = plugin.config.getString("Spawn_coords").split(",");
 		World spawnw = plugin.getServer().getWorld(Spawncoords[3]);
 		double spawnx = Double.parseDouble(Spawncoords[0]);
 		double spawny = Double.parseDouble(Spawncoords[1]);
 		double spawnz = Double.parseDouble(Spawncoords[2]);
 		Location Spawn = new Location(spawnw, spawnx, spawny, spawnz);
+		if(plugin.Frozen.contains(pname) && plugin.Playing.contains(pname)){
+			event.setDeathMessage("");
+			p.getServer().broadcastMessage(pname + ChatColor.LIGHT_PURPLE + " Stepped off their pedestal too early!");
+			plugin.Frozen.remove(pname);
+			plugin.Playing.remove(pname);
+			plugin.Dead.add(pname);
+			s.broadcastMessage(leftmsg);
+			if(plugin.Playing.size()== 1 && plugin.canjoin== true){
+				//Announce winner
+				String winnername = plugin.Playing.get(i++);
+				Player winner = plugin.getServer().getPlayerExact(winnername);
+				String winnername2 = winner.getName();
+				plugin.getServer().broadcastMessage(ChatColor.GREEN + winnername2 + " is the victor of this Hunger Games!");
+				winner.getInventory().clear();
+				winner.teleport(Spawn);
+				winner.getInventory().setBoots(null);
+				winner.getInventory().setChestplate(null);
+				winner.getInventory().setHelmet(null);
+				winner.getInventory().setLeggings(null);
+				winner.getInventory().addItem(plugin.Reward);
+				plugin.Playing.clear();
+				//Show spectators
+				if(!plugin.Watching.isEmpty()){
+					String s1 = plugin.Watching.get(i++);
+					Player spectator = plugin.getServer().getPlayerExact(s1);
+					spectator.setAllowFlight(false);
+					spectator.teleport(Spawn);
+					for(Player online:plugin.getServer().getOnlinePlayers()){
+						online.showPlayer(spectator);
+					}
+				}
+				if(plugin.config.getString("Auto_Restart").equalsIgnoreCase("True")){
+					Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "ha restart");
+				}
+			}
+		}
 		if(plugin.Playing.contains(pname)){
 			if(plugin.config.getString("Cannon_Death").equalsIgnoreCase("True")){
 				double y = p.getLocation().getY();
@@ -51,7 +88,6 @@ public class DeathListener implements Listener{
 			}
 			plugin.Dead.add(pname);
 			plugin.Playing.remove(pname);
-			String leftmsg = ChatColor.BLUE + "There are now " + plugin.Playing.size() + " tributes left!";
 			if(p.getKiller() instanceof Player){
 				if(p.getKiller().getItemInHand().getType().getId()== 0){
 					Player killer = p.getKiller();
@@ -141,10 +177,10 @@ public class DeathListener implements Listener{
 					winner.getInventory().setHelmet(null);
 					winner.getInventory().setLeggings(null);
 					winner.getInventory().addItem(plugin.Reward);
-                                        // Create the event here
-                                        PlayerWinGamesEvent winevent = new PlayerWinGamesEvent(winner);
-                                        // Call the event
-                                        Bukkit.getServer().getPluginManager().callEvent(winevent);
+					// Create the event here
+					PlayerWinGamesEvent winevent = new PlayerWinGamesEvent(winner);
+					// Call the event
+					Bukkit.getServer().getPluginManager().callEvent(winevent);
 					plugin.Playing.clear();
 					//Show spectators
 					if(!plugin.Watching.isEmpty()){
